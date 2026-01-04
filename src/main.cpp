dc8df448377e8440cc48a8777f24ab09f62de138
Zachary Bunch <zacharybunch1284@gmail.com>
2026-01-03 21:18:57 -0500

    Included safer object pointer inits

--- src/main.cpp
+++ src/main.cpp
@@ -24,12 +24,12 @@ Scene scene;
 
 PhysicsEngine physics;
 
-Object cube("../res/cube.obj", BOX, 1.0f);
+Object* cube = new Object{"../res/cube.obj", BOX};
 
-Object floorObject("../res/cube.obj", BOX, 1.0f);
+Object* floorObject = new Object{"../res/cube.obj", BOX};
 
-Light light(glm::vec3(3.0f, 10.0f, 4.0f), glm::vec3(1.0f, 10.0f, 1.0f), 1, glm::vec3(0.0f, 0.0f, 0.0f));
-Light light2(glm::vec3(-5.0f, 10.0f, -5.0f), glm::vec3(0.2f, 0.75f, 0.2f), 1.0f, glm::vec3(0.0f));
+Light* light = new Light{glm::vec3(3.0f, 10.0f, 4.0f), glm::vec3(1.0f, 10.0f, 1.0f), 1, glm::vec3(0.0f, 0.0f, 0.0f)};
+Light* light2 = new Light{glm::vec3(-5.0f, 10.0f, -5.0f), glm::vec3(0.2f, 0.75f, 0.2f), 1, glm::vec3(0.0f)};
 
 int main()
 {   
@@ -46,25 +46,25 @@ int main()
 
     glClearColor(0.15f, 0.2f, 0.3f, 1.0f);
 
-    cube.Position.y = 20.0f;
-    cube.affectedByGravity = true;
-    cube.rigidBody.InitRigidBody(cube.Position, cube.Velocity);
+    cube->Position.y = 20.0f;
+    cube->affectedByGravity = true;
+    cube->rigidBody.InitRigidBody(cube->Position, cube->Velocity);
 
-    floorObject.Position.y = 1.0f;
-    floorObject.Scale.y *= 0.5f;
-    floorObject.Scale.x *= 10.0f;
-    floorObject.Scale.z *= 10.0f;
-    floorObject.rigidBody.InitRigidBody(floorObject.Position, floorObject.Velocity);
-    floorObject.affectedByGravity = false;
+    floorObject->Position.y = 1.0f;
+    floorObject->Scale.y *= 0.5f;
+    floorObject->Scale.x *= 10.0f;
+    floorObject->Scale.z *= 10.0f;
+    floorObject->rigidBody.InitRigidBody(floorObject->Position, floorObject->Velocity);
+    floorObject->affectedByGravity = false;
 
-    scene.AddObject(std::make_unique<Object>(cube));
-    scene.AddObject(std::make_unique<Object>(floorObject));
-    scene.AddLight(std::make_unique<Light>(light));
+    scene.AddObject(std::unique_ptr<Object>(cube));
+    scene.AddObject(std::unique_ptr<Object>(floorObject));
+    scene.AddLight(std::unique_ptr<Light>(light));
     //scene.RemoveLight(&light);
     //scene.AddLight(&light2);
 
-    cube.rigidBody.collider = SPHERE;
-    floorObject.rigidBody.collider = SPHERE;
+    cube->rigidBody.collider = SPHERE;
+    floorObject->rigidBody.collider = SPHERE;
 
     while(glfwGetKey(window.getWindow(), GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
            glfwWindowShouldClose(window.getWindow()) == 0 )
@@ -81,7 +81,7 @@ int main()
 
         glm::mat4 MVPmat = projectionMat * viewMat;
 
-        unsigned int MVPID = glGetUniformLocation(shaderID, "MVP");
+        const unsigned int MVPID = glGetUniformLocation(shaderID, "MVP");
         glUniformMatrix4fv(MVPID, 1, GL_FALSE, &MVPmat[0][0]);
 
         scene.RenderScene();
